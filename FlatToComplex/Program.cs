60c2746c57ab7b1784c568a15066475d273d1678
Stephen Hauck <stephenhauck@users.noreply.github.com>
2021-10-17 09:53:35 -0400

    Initial commit

---
+++ FlatToComplex/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace FlatToComplex
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var employee = new Employee()
+            {
+                Name = "Bob Jones",
+                Salary = 2000,
+                Department = "Human resources",
+                City = "San Diego",
+                State = "CA",
+                Country = "United States"
+            };
+
+            //Serialize object
+            var employeeDTOTest = JsonConvert.DeserializeObject<EmployeeDTO>(JsonConvert.SerializeObject(employee));
+
+        }
+    }
+
+    public class Employee
+    {
+
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Department { get; set; }
+        //properties in the Address object
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+
+
+        //This makes the Employee object transform when serialized
+        [JsonProperty("address")]
+        private Address address { get; set; }
+
+        [OnSerializing]
+        internal void OnSerializingMethod(StreamingContext context)
+        {
+            address = new Address { City = this.City, State = this.State, Country = this.Country };
+        }
+
+    }
+
+    public class EmployeeDTO
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Department { get; set; }
+        public Address address { get; set; }
+    }
+
+    public class Address
+    {
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+    }
+}
